0b96eafc9ddf09278c772856d22d94985a50e15e
ischenkx <formypsnspb@gmail.com>
2023-04-21 16:12:03 +0400

    init

---
+++ client/client.py
@@ -0,0 +1,25 @@
+class API:
+    def get(self, path: str, data: any):
+        pass
+
+
+class Client:
+    def __init__(self):
+        pass
+
+    @property
+    def stage(self):
+        pass
+
+    @property
+    def factory(self):
+        pass
+
+    @property
+    def colors(self):
+        pass
+
+    @property
+    def ballista(self):
+        pass
+
